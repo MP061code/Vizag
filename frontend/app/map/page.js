4e3b90d0fb2e5c99f936f9ec3abe5356a8311803
MPcode061 <mukesh061062@gmail.com>
2026-03-31 02:37:55 +0530

    fix: leaflet SSR issue for Vercel build

--- frontend/app/map/page.js
+++ frontend/app/map/page.js
@@ -1,13 +1,37 @@
 "use client";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
+
+// Load Map ONLY on client side
+const MapContainer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.MapContainer),
+  { ssr: false }
+);
+
+const TileLayer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.TileLayer),
+  { ssr: false }
+);
+
+const Marker = dynamic(
+  () => import("react-leaflet").then((mod) => mod.Marker),
+  { ssr: false }
+);
+
+const Popup = dynamic(
+  () => import("react-leaflet").then((mod) => mod.Popup),
+  { ssr: false }
+);
 
 export default function MapPage() {
   return (
-    <MapContainer center={[17.6868, 83.2185]} zoom={13} style={{ height: "100vh" }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+    <MapContainer
+      center={[17.6868, 83.2185]}
+      zoom={13}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <Marker position={[17.6868, 83.2185]}>
         <Popup>Garbage Reported Here</Popup>
       </Marker>
